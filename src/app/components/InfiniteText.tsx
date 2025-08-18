fbdf38fc9b01592dd66954cad4147df3e2048347
Oyunbat <oyunbat9958@gmail.com>
2025-08-18 22:34:17 +0800

    header not done googlefont added , slider ref fixed

--- src/app/components/InfiniteText.tsx
+++ src/app/components/InfiniteText.tsx
@@ -1,9 +1,16 @@
 "use client"
 import gsap from "gsap";
 import React from "react";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef } from "react";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { Bebas_Neue, Playfair_Display } from "next/font/google";
+
+const bebas = Bebas_Neue({
+    subsets: ["latin"],
+    weight: "400"
+})
 export default function InfiniteText() {
+
     const firstText = useRef<HTMLParagraphElement>(null);
     const secondText = useRef<HTMLParagraphElement>(null);
     const thirdText = useRef<HTMLParagraphElement>(null);
@@ -28,19 +35,17 @@ export default function InfiniteText() {
     const animation = () => {
         if (xPercent <= -100) xPercent = 0;
         if (xPercent >= 100) xPercent = 0;
-        gsap.set(firstText.current, { xPercent: xPercent })
-        gsap.set(secondText.current, { xPercent: xPercent })
-        gsap.set(thirdText.current, { xPercent: xPercent })
+        gsap.set(slider.current, { xPercent: xPercent })
 
-        xPercent += 0.1 * direction;
+        xPercent += 0.2 * direction;
         requestAnimationFrame(animation)
     }
 
     return (
-        <div className="text-[#fff] flex relative bg-red-200 md:bg-amber-200 xl:bg-green-200 w-full font-serif  ">
-            <p ref={firstText} className="text-[52px] md:text-[90px] xl:text-[150px] absolute -left-130 md:-left-220 xl:-left-350"> - Oyunbat-Batnasan </p>
-            <p ref={secondText} className="text-[52px] md:text-[90px] xl:text-[150px] ">  Oyunbat-Batnasan </p>
-            <p ref={thirdText} className="text-[52px] md:text-[90px] xl:text-[150px] absolute -right-110 md:-right-220 xl:-right-320"> Oyunbat-Batnasan </p>
-        </div>
+        <div ref={slider} className={`text-[#fff] flex relative  md:bg-amber-200 xl:bg-green-200 w-full justify-center items-center ${bebas.className}`}>
+            <p ref={firstText} className="text-[62px] md:text-[100px] xl:text-[170px] absolute -left-100 md:-left-220 xl:-left-280"> - Oyunbat-Batnasan </p>
+            <p ref={secondText} className={`text-[62px] md:text-[100px] xl:text-[170px] absolue`}>  Oyunbat-Batnasan </p>
+            <p ref={thirdText} className="text-[62px] md:text-[100px] xl:text-[170px] absolute -right-100 md:-right-220 xl:-right-260"> Oyunbat-Batnasan </p>
+        </div >
     )
 }
