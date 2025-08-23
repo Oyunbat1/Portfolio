fd47929d1756e33a17e6e936967dadd0e432ad6c
Oyunbat <oyunbat9958@gmail.com>
2025-08-23 17:01:37 +0800

    project hover

--- src/app/components/projects/page.tsx
+++ src/app/components/projects/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { useState, useEffect, useRef } from 'react';
 import Project from './project/ProjectItem';
-import { Easing } from 'framer-motion';
+import Image from 'next/image';
+import { Easing, motion } from 'framer-motion';
 import gsap from 'gsap';
 import { Barlow_Condensed } from "next/font/google";
-
+import RoundedButton from '@/app/common/RoundedButton';
+import { style } from 'framer-motion/client';
 const barlow = Barlow_Condensed({
     subsets: ["latin"],
     weight: "400"
@@ -52,8 +54,8 @@ export default function Home() {
     const yMoveContainer = useRef<((value: number) => gsap.core.Tween) | null>(null);
     const xMoveCursor = useRef<((value: number) => gsap.core.Tween) | null>(null);
     const yMoveCursor = useRef<((value: number) => gsap.core.Tween) | null>(null);
-    const xMoveCursorLabel = useRef<((value: number) => gsap.core.Tween) | null>(null);
-    const yMoveCursorLabel = useRef<((value: number) => gsap.core.Tween) | null>(null);
+    // const xMoveCursorLabel = useRef<((value: number) => gsap.core.Tween) | null>(null);
+    // const yMoveCursorLabel = useRef<((value: number) => gsap.core.Tween) | null>(null);
     const [isMobile, setIsMobile] = useState(false)
     const [isTablet, setIsTablet] = useState(false)
     console.log(isTablet, "HEREEE")
@@ -65,8 +67,9 @@ export default function Home() {
         xMoveCursor.current = gsap.quickTo(cursor.current, "left", { duration: 0.5, ease: "power3" })
         yMoveCursor.current = gsap.quickTo(cursor.current, "top", { duration: 0.5, ease: "power3" })
 
-        xMoveCursorLabel.current = gsap.quickTo(cursorLabel.current, "left", { duration: 0.45, ease: "power3" })
-        yMoveCursorLabel.current = gsap.quickTo(cursorLabel.current, "top", { duration: 0.45, ease: "power3" })
+        // xMoveCursorLabel.current = gsap.quickTo(cursorLabel.current, "left", { duration: 0.45, ease: "power3" })
+        // yMoveCursorLabel.current = gsap.quickTo(cursorLabel.current, "top", { duration: 0.45, ease: "power3" })
+
         const handleResize = () => {
             const width = window.innerWidth;
             setIsMobile(width <= 768);
@@ -74,22 +77,23 @@ export default function Home() {
         };
         handleResize();
         window.addEventListener("resize", handleResize);
-
         return () => { window.removeEventListener("resize", handleResize) }
     }, [])
 
 
     const moveItems = (x: number, y: number) => {
-
         xMoveContainer.current?.(x);
         yMoveContainer.current?.(y);
         xMoveCursor.current?.(x);
         yMoveCursor.current?.(y);
-        xMoveCursorLabel.current?.(x);
-        yMoveCursorLabel.current?.(y);
+        // xMoveCursorLabel.current?.(x);
+        // yMoveCursorLabel.current?.(y);
     };
     const manageModal = (active: boolean, index: number, x: number, y: number) => {
         setModal((prev) => ({ ...prev, active, index, x, y }));
+        if (active) {
+            moveItems(x, y)
+        }
     };
 
     return (
@@ -104,63 +108,26 @@ export default function Home() {
                     })
                 }
             </div>
-            {/* <Rounded>
-                <p>More work</p>
-            </Rounded> */}
-            {/* <>
-                <motion.div
-                    ref={modalContainer}
-                    initial={{ opacity: 0, scale: 0 }}
-                    animate={active ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0 }}
-                    transition={{ duration: 0.3, ease: easing }}
-                    className="h-[350px] w-[400px] fixed bg-white pointer-events-none overflow-hidden z-[3]"
-                    style={{
-                        left: x - 400 / 2,
-                        top: y - 350 / 2
-                    }}
-                >
-                    <div
-                        style={{ top: index * -100 + "%" }}
-                        className="h-full w-full relative transition-[top] duration-500 ease-[cubic-bezier(0.76,0,0.24,1)]"
-                    >
-                        {projects.map((project, idx) => (
-                            <div
-                                className="h-full w-full flex items-center justify-center"
-                                style={{ backgroundColor: project.color }}
-                                key={`modal_${idx}`}
-                            >
+            <motion.div ref={modalContainer} variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"} className='h-[350px] w-[400px]  fixed top-1/2 left-1/2 bg-white  overflow-hidden z-10'>
+                <div style={{ top: index * -100 + "%" }} className='h-full w-full relative transition-[top] duration-500 ease-[cubic-bezier(0.76,0,0.24,1)]'>
+                    {
+                        projects.map((project, index) => {
+                            const { src, color } = project
+                            return <div className='h-full w-full flex items-center justify-center' style={{ backgroundColor: color }} key={`modal_${index}`}>
                                 <Image
-                                    src={`/${project.src}`}
+                                    src={`/${src}`}
                                     width={300}
                                     height={0}
                                     alt="image"
-                                    className="h-auto"
+                                    className='h-auto'
                                 />
                             </div>
-                        ))}
-                    </div>
-                </motion.div>
+                        })
+                    }
+                </div>
+            </motion.div>
 
-                {/* <motion.div
-                    ref={cursor}
-                    className="w-[80px] h-[80px] rounded-full bg-[#455CE9] text-white fixed z-[3] flex items-center justify-center   text-[14px] font-[300] pointer-events-none"
-
-                    variants={scaleAnimation}
-                    initial="initial"
-                    animate={active ? "enter" : "closed"}
-                >
-                    <motion.div
-                        className='  ml-6.5 mt-[20] '
-                        ref={cursorLabel}
-                        variants={scaleAnimation}
-                        initial="initial"
-                        animate={active ? "enter" : "closed"}
-                    >
-                        View
-                    </motion.div>
-                </motion.div> */}
-
-            {/* </> */}
+            <motion.div ref={cursor} className="w-[80px] h-[80px] rounded-full bg-[#455CE9] text-white fixed items-center justify-center font-[300] z-20" variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"}></motion.div>
         </main >
     )
 }
